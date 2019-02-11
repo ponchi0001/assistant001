defd3afcda583ea44e3a20c83f44166c3aabb0ae
ponchi0001 <srsmith1214@gmail.com>
2019-02-11 23:29:04 +0900

    Newfile
    Dotima

---
+++ bin/DailyLine_Dotima.py
@@ -0,0 +1,33 @@
+#coding:UTF-8
+import requests
+import json
+import datetime
+
+#mobileDotimaの時間を通知する
+def main(msg):
+    url = "https://notify-api.line.me/api/notify"
+    #ファイルを読み込んでアシスタント決定
+    path = '/home/pi/table/sTABLE'
+    with open(path) as f:
+        to = f.readlines()
+    #宛先はアシスタント
+    token = to[0].rstrip()
+    headers = {"Authorization" : "Bearer "+ token}
+
+    message =  msg
+    payload = {"message" :  message}
+#    files = {"imageFile": open("test.jpg", "rb")} #バイナリで画像ファイルを開きます。対応している形式はPNG/JPEGです.
+    r = requests.post(url ,headers = headers ,params=payload)
+    
+def getapi():
+#print("15分以内にモバイルDotimaの開始ボタンを押してください。")
+    path = '/home/pi/table/mTABLE'
+    with open(path) as f:
+        mo = f.readlines()
+#宛先はアシスタント
+    msg = mo[0].rstrip()
+    return msg
+
+if __name__ == '__main__':
+    msg = getapi()
+    main(msg)
