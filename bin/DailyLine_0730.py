d197eea7a51468564431990710767eaef3c97932
ponchi0001 <srsmith1214@gmail.com>
2019-02-17 23:12:58 +0900

    update
    update

--- bin/DailyLine_0730.py
+++ bin/DailyLine_0730.py
@@ -5,11 +5,14 @@ import json
 #朝の7時半に天気予報の概要をLINEするためのスクリプト
 def main(msg):
     url = "https://notify-api.line.me/api/notify"
-#宛先はアシスタント
-    token = "DE2O0rewCeKShOPQ4jZBPhZE1dW9naQvZw9ufsz5nYv"#ここにアクセストークンを入力します。
-    #token = "HmRFH319MLcN6WLTY1wpeCw4Q5kSQ1YOhER4dJNcbRb"#ここにアクセストークンを入力します。
+    #ファイルを読み込んでアシスタント決定
+    path = '/home/pi/work/table/sTABLE'
+    with open(path) as f:
+        to = f.readlines()
+    #宛先はアシスタント
+    token = to[0].rstrip()
     headers = {"Authorization" : "Bearer "+ token}
-
+    #メッセージを作る。
     message =  msg
     payload = {"message" :  message}
 #    files = {"imageFile": open("test.jpg", "rb")} #バイナリで画像ファイルを開きます。対応している形式はPNG/JPEGです.
--- bin/DailyLine_0801.py
+++ bin/DailyLine_0801.py
@@ -6,9 +6,14 @@ import datetime
 #8時に曜日毎の日課を通知するスクリプト
 def main(msg):
     url = "https://notify-api.line.me/api/notify"
-    token = "DE2O0rewCeKShOPQ4jZBPhZE1dW9naQvZw9ufsz5nYv"#ここにアクセストークンを入力します。
+    #ファイルを読み込んでアシスタント決定
+    path = '/home/pi/work/table/sTABLE'
+    with open(path) as f:
+        to = f.readlines()
+    #宛先はアシスタント
+    token = to[0].rstrip()
     headers = {"Authorization" : "Bearer "+ token}
-
+    #メッセージを作成する。
     message =  msg
     payload = {"message" :  message}
 #    files = {"imageFile": open("test.jpg", "rb")} #バイナリで画像ファイルを開きます。対応している形式はPNG/JPEGです.
@@ -20,13 +25,18 @@ def getapi():
 #print("今日は"+Strweekday[aDate.weekday()]+"曜日です。")
 #各曜日毎のタスクを定義する。
 	task=["","","","","","",""]
-	task[0]=["本気の英単語1分チャレンジ各レベル×３回"]
-	task[1]=["本気の英単語1分チャレンジ各レベル×３回"]
-	task[2]=["本気の英単語1分チャレンジ各レベル×３回","パジャマ交換"]
-	task[3]=["本気の英単語1分チャレンジ各レベル×３回"]
-	task[4]=["本気の英単語1分チャレンジ各レベル×３回"]
-	task[5]=["パジャマ交換","爪切り"]
-	task[6]=["スーツのアイロン掛け","眉毛の手入れ"]
+    path = '/home/pi/work/table/mTABLEWeektasks'
+    with open(path) as f:
+        mo = f.readlines()
+        #	task[0]=["本気の英単語1分チャレンジ各レベル×３回"]
+        #	task[1]=["本気の英単語1分チャレンジ各レベル×３回"]
+        #	task[2]=["本気の英単語1分チャレンジ各レベル×３回","パジャマ交換"]
+        #	task[3]=["本気の英単語1分チャレンジ各レベル×３回"]
+        #	task[4]=["本気の英単語1分チャレンジ各レベル×３回"]
+        #	task[5]=["パジャマ交換","爪切り"]
+        #	task[6]=["スーツのアイロン掛け","眉毛の手入れ"]
+for i in range(7):
+    task[i]=mo(i)
 #print("今日の定型タスクは以下です。")
 	msg = '\n' + \
 	"今日は"+Strweekday[aDate.weekday()]+"曜日です。" + '\n' \
