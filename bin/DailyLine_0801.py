6139fb24a5868fddbc619d5e4f8c7bdd12b30f88
ponchi0001 <srsmith1214@gmail.com>
2019-02-17 23:30:43 +0900

    add new file

--- bin/DailyLine_0801.py
+++ bin/DailyLine_0801.py
@@ -6,14 +6,9 @@ import datetime
 #8時に曜日毎の日課を通知するスクリプト
 def main(msg):
     url = "https://notify-api.line.me/api/notify"
-    #ファイルを読み込んでアシスタント決定
-    path = '/home/pi/work/table/sTABLE'
-    with open(path) as f:
-        to = f.readlines()
-    #宛先はアシスタント
-    token = to[0].rstrip()
+    token = "DE2O0rewCeKShOPQ4jZBPhZE1dW9naQvZw9ufsz5nYv"#ここにアクセストークンを入力します。
     headers = {"Authorization" : "Bearer "+ token}
-    #メッセージを作成する。
+
     message =  msg
     payload = {"message" :  message}
 #    files = {"imageFile": open("test.jpg", "rb")} #バイナリで画像ファイルを開きます。対応している形式はPNG/JPEGです.
@@ -25,20 +20,13 @@ def getapi():
 #print("今日は"+Strweekday[aDate.weekday()]+"曜日です。")
 #各曜日毎のタスクを定義する。
 	task=["","","","","","",""]
-    path = '/home/pi/work/table/mTABLEWeektasks'
-
-    with open(path) as f:
-        mo = f.readlines()
-        #	task[0]=["本気の英単語1分チャレンジ各レベル×３回"]
-        #	task[1]=["本気の英単語1分チャレンジ各レベル×３回"]
-        #	task[2]=["本気の英単語1分チャレンジ各レベル×３回","パジャマ交換"]
-        #	task[3]=["本気の英単語1分チャレンジ各レベル×３回"]
-        #	task[4]=["本気の英単語1分チャレンジ各レベル×３回"]
-        #	task[5]=["パジャマ交換","爪切り"]
-        #	task[6]=["スーツのアイロン掛け","眉毛の手入れ"]
-    for i in range(7):
-        task[i]=mo(i)
-
+	task[0]=["本気の英単語1分チャレンジ各レベル×３回"]
+	task[1]=["本気の英単語1分チャレンジ各レベル×３回"]
+	task[2]=["本気の英単語1分チャレンジ各レベル×３回","パジャマ交換"]
+	task[3]=["本気の英単語1分チャレンジ各レベル×３回"]
+	task[4]=["本気の英単語1分チャレンジ各レベル×３回"]
+	task[5]=["パジャマ交換","爪切り"]
+	task[6]=["スーツのアイロン掛け","ゴルフの練習","眉毛の手入れ"]
 #print("今日の定型タスクは以下です。")
 	msg = '\n' + \
 	"今日は"+Strweekday[aDate.weekday()]+"曜日です。" + '\n' \
