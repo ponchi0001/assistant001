04307546bc1ac84cc87ba79c22098f6677d96a5f
ponchi0001 <srsmith1214@gmail.com>
2019-03-04 01:00:52 +0900

    update
    update

---
+++ bin/DailyLine_1700.py
@@ -0,0 +1,34 @@
+#coding:UTF-8
+import requests
+import json
+
+#毎日朝8時の通知を行うスクリプト。
+def main(msg):
+    url = "https://notify-api.line.me/api/notify"
+    #ファイルを読み込んでアシスタント決定
+    path = '/home/pi/work/table/sTABLE'
+    with open(path) as f:
+        to = f.readlines()
+    #宛先はアシスタント
+    token = to[0].rstrip()
+    headers = {"Authorization" : "Bearer "+ token}
+
+    message =  msg
+    payload = {"message" :  message}
+#    files = {"imageFile": open("test.jpg", "rb")} #バイナリで画像ファイルを開きます。対応している形式はPNG/JPEGです.
+    r = requests.post(url ,headers = headers ,params=payload)
+    
+def getapi():
+    path = '/home/pi/work/table/mTABLE1700'
+    with open(path) as f:
+        mo = f.readlines()
+    #宛先はアシスタント
+    msg = ""
+    for mm in mo:
+        msg = msg + mm
+    #print(msg)
+    return msg
+
+if __name__ == '__main__':
+    msg = getapi()
+    main(msg)
